81261a67d20e9ae99e8a329d7fbaed4cb98e85cf
ApoTed <j.tedeschi@studenti.unibs.it>
2022-09-14 15:39:57 +0200

    solo costruzione interfaccie temporanea

--- src/it/unibs/IngSftwB/Model/Applicazione.java
+++ src/it/unibs/IngSftwB/Model/Applicazione.java
@@ -3,6 +3,9 @@ package it.unibs.IngSftwB.Model;
 // temporanea per implementare accesso
 public class Applicazione {
 
+    private Offerte offerte;
+    private ListaScambi listaScambi;
+    private Configurazione configurazione;
     private DatiUtenti datiUtenti;
 
     public Applicazione(DatiUtenti datiUtenti) {
