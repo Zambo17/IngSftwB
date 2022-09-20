864e9f6b0b8a0c0be9ec80671cd519bf67b8b26b
Zambo17 <e.zambelli008@studenti.unibs.it>
2022-09-20 18:54:06 +0200

    Iniziato modifica parametri

---
+++ src/it/unibs/IngSftwB/Controller/AzioniConfiguratore/StampaOfferteFoglia.java
@@ -0,0 +1,34 @@
+package it.unibs.IngSftwB.Controller.AzioniConfiguratore;
+
+import it.unibs.IngSftwB.Controller.*;
+import it.unibs.IngSftwB.Model.Applicazione;
+import it.unibs.IngSftwB.Model.Categoria;
+import it.unibs.IngSftwB.Model.Offerte;
+import it.unibs.IngSftwB.Model.Utente;
+
+public class StampaOfferteFoglia implements AzioneUtente {
+    @Override
+    public void eseguiAzione(Controller controller, Utente utente) {
+        Applicazione app=controller.getApp();
+
+        Categoria[] categoriaFoglia= app.getConfigurazione().getSis().scegliFoglia();
+        if(categoriaFoglia[0] != null){
+            Offerte tosee=app.getOfferte().offerteFoglia(categoriaFoglia[0].getNome(), categoriaFoglia[1].getNome());
+            if(tosee.getListaOfferte().size()!=0){
+                controller.comunicaAllaView(MessaggioGenerale.OFFERTE_CATEGORIA);
+                controller.getView().stampaOfferteDescription((MessaggioOfferte) app.getOfferte().getOfferteDefinition());
+            }
+            else{
+                controller.comunicaAllaView(MessaggioErrore.NO_OFFERTE_APERTE);
+            }
+        }
+        else{
+            controller.comunicaAllaView(MessaggioErrore.VISUALIZZAZIONE_OFFERTE_FALLITA);
+        }
+    }
+
+    @Override
+    public String getNomeAzione() {
+        return "Visualizza le offerte di una categoria";
+    }
+}
